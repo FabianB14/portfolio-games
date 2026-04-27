273cb8adc8bb8887f55c78e251fdd320e145ade6
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2026-04-27 11:17:36 -0700

    Update base path in Vite configuration

--- fabian-brooks-portfolio/vite.config.js
+++ fabian-brooks-portfolio/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'fabianb14.github.io/portfolio-games/',
+  base: '/portfolio-games/',
   plugins: [react()],
 })
